95ce30b44edb1d7ad234b722366877fa413be0a8
knochen336 <152942973+knochen336@users.noreply.github.com>
2024-08-05 22:26:57 +0800

    Add files via upload

---
+++ ieee 30 case/net/fcn_对比实验.py
@@ -0,0 +1,33 @@
+import numpy as np
+from torch.nn.utils import weight_norm
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+x_length = 12
+y_length = 12
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.fc = torch.nn.Linear(30 * x_length, y_length * 64)
+        self.fc2 = torch.nn.Linear(64 * x_length, y_length * 32)
+        self.fc3 = torch.nn.Linear(32 * x_length, y_length *16)
+        self.fc4 = torch.nn.Linear(16 * x_length, y_length * 6)
+
+    def forward(self, data):
+        x, edge_index, edge_weight = data.x, data.edge_index, data.edge_weight
+
+        x = torch.flatten(x, start_dim=1)  # 展开张量
+        x = x.view(-1, 30 * x_length)  # 将数据的形状变为 [batch_size, 64*30]
+        x = self.fc(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        x = F.relu(x)
+        x = self.fc4(x)
+        x = F.relu(x)
+        return x
+
